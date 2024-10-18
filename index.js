26d1ae952e669e2c6b7522602d46580fea7bbed1
manishraj27 <manish012raj@gmail.com>
2024-10-18 15:48:11 +0530

    Refactor import statement for chalk package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
-const chalk = require('chalk');
+import chalk from 'chalk';
 const { execSync } = require("child_process");
 
 function checkNodeVersion() {
