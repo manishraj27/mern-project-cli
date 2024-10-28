3ddadbe2fe5e013af6beb24a3d303213b7ac50a0
MD Rehan <mdrehan4650@gmail.com>
2024-10-29 00:16:55 +0530

    Bug Fixed of middleware file

--- commands/createNextJs.js
+++ commands/createNextJs.js
@@ -16,6 +16,29 @@ export async function middleware(req) {
         const response = NextResponse.next();
         return response;
 
+    } catch (err) {
+        return NextResponse.next({ "message": "Server Error", "error": err?.message }, { status: 500 })
+    }
+}
+
+export const config = {
+    matcher: [
+        '/'
+    ]
+}
+`;
+
+const middlewareContentTS = `
+"use server"
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    try {
+
+        const response = NextResponse.next();
+        return response;
+
     } catch (err: any) {
         return NextResponse.next({ "message": "Server Error", "error": err?.message }, { status: 500 })
     }
@@ -32,6 +55,7 @@ export default function createNextJsProject(projectName) {
   checkNodeVersion();
 
   let rootDir = '';
+  let appPath = '';
   if (projectName === '.') rootDir = process.cwd();
   else rootDir = path.join(process.cwd(), projectName);
 
@@ -73,19 +97,19 @@ export default function createNextJsProject(projectName) {
       ];
 
       const appDirsToCreate = ['(main)', '(auth)', 'api'];
+      appPath = '';
+      let isSrcDir = fs.existsSync(path.join(rootDir, 'src'));
+      
+      if (isSrcDir) appPath = path.join(rootDir, 'src');
+      else appPath = rootDir
 
       dirsToCreate.forEach((dir) => {
-        const appDir = path.join(rootDir, 'src', dir);
+        const appDir = path.join(appPath, dir);
         fs.mkdirSync(appDir, { recursive: true });
       });
 
-      let isAppDir = fs.existsSync(path.join(rootDir, 'src', 'app'));
-      let appPath = '';
-      if (isAppDir) appPath = path.join(rootDir, 'src', 'app');
-      else appPath = path.join(rootDir, 'src');
-
       appDirsToCreate.forEach((dir) => {
-        const appDir = path.join(appPath, dir);
+        const appDir = path.join(appPath, 'app', dir);
         fs.mkdirSync(appDir, { recursive: true });
       });
     },
@@ -99,8 +123,12 @@ export default function createNextJsProject(projectName) {
     () => {
       let fileName = 'middleware.js';
       const isTSProject = fs.existsSync(path.join(rootDir, 'tsconfig.json'));
-      if (isTSProject) fileName = 'middleware.ts';
-      fs.writeFileSync(path.join(rootDir, 'src', fileName), middlewareContent);
+      if (isTSProject) {
+        fileName = 'middleware.ts';
+        fs.writeFileSync(path.join(appPath, fileName), middlewareContentTS);
+      } else {
+        fs.writeFileSync(path.join(appPath, fileName), middlewareContent);
+      }
     },
     '🔧 Creating Middleware file...',
     '✅ Middleware created successfully!',
