9ea039d6f23029af36c3ad30885e5313be052a5b
manishraj27 <manish012raj@gmail.com>
2025-03-19 14:14:08 +0530

    Bump version to 2.1.6 in package.json and add ESLint alias in createFrontend.js

--- commands/createFrontend.js
+++ commands/createFrontend.js
@@ -123,6 +123,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
